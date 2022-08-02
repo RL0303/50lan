efb78890ab8457daa2bab18737ef115b1b9e9a7f
Robert <linrobert0303@gmail.com>
2022-08-02 22:43:01 +0800

    Initial Commit

---
+++ 50lan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  50lan
+//
+//  Created by 林辰澤 on 2022/8/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
